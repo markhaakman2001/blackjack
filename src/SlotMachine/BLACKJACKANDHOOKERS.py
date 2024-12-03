9c1e44b030d35c7ccc0384b6172db035ff63a1e2
Niels Hoogerwerf <n.hoogerwerf@student.tudelft.nl>
2024-12-03 23:42:27 +0100

    simple example to work with frames and layouts. However no animation

---
+++ src/SlotMachine/BLACKJACKANDHOOKERS.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2013 Riverbank Computing Limited.
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+from __future__ import annotations
+
+from PySide6.QtCore import Qt
+
+"""PySide6 port of the widgets/layouts/basiclayout example from Qt v5.x"""
+
+import sys
+
+from PySide6.QtWidgets import (QApplication, QComboBox, QDialog,
+                               QDialogButtonBox, QGridLayout, QGroupBox,
+                               QFormLayout, QHBoxLayout, QLabel, QLineEdit,
+                               QMenu, QMenuBar, QPushButton, QSpinBox,
+                               QTextEdit, QVBoxLayout, QFrame)
+
+
+class Slots(QDialog):
+    num_grid_rows = 3
+
+    def __init__(self):
+        super().__init__()
+
+        self.create_grid_group_box()
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self._grid_group_box)
+        self.setLayout(main_layout)
+        self.setWindowTitle("SLOTS")
+        self.showMaximized()
+
+    def create_grid_group_box(self):
+        self._grid_group_box = QGroupBox("Grid layout")
+        layout = QGridLayout()
+
+        for i in range(Slots.num_grid_rows):
+            for j in range(3):
+                placeHolder1 = QLabel("SIMPLESLOTHOLDER")
+                placeHolder1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                placeHolder1.setFrameStyle(QFrame.Panel)
+                layout.addWidget(placeHolder1, i, j)
+
+        self._grid_group_box.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    slots = Slots()
+    sys.exit(slots.exec())
