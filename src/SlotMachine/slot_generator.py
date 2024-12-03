520d683859fd0f90e36245636dce9313ac6e7ef7
markhaakman2001 <135602514+markhaakman2001@users.noreply.github.com>
2024-12-03 13:03:30 +0100

    Update slot_generator.py

--- src/SlotMachine/slot_generator.py
+++ src/SlotMachine/slot_generator.py
@@ -6,6 +6,8 @@ import random
 class Reels:
 
     def __init__(self):
+
+        # What the slot machine has to display, instead of integers
         self.possible_values = { 
             1 : "10",
             2 : "J",
@@ -23,17 +25,22 @@ class Reels:
         self.reel_values = np.array(slots)
         self.reel_disp = np.array(slots)
     
+
     def generate_reel(self):
 
         new_reel = []
+
+        # choose a random digit for every slot in the reel
         for x in range(5):
             x = random.randint(1, 8)
             new_reel.append(x)
 
+        # array values
         self.reel_values = np.array(new_reel)
+        
         self.reel_disp = np.array([self.possible_values.get(x) for x in self.reel_values])
         
-        
+
 
 class PlayingField:
 
@@ -52,20 +59,25 @@ class PlayingField:
 
             print(self.full_field)
             print(self.full_field_disp)
+            for i, slot in enumerate(self.full_field):
+                print(i, slot)
         
-
-            
-
+    def printaline(self, row_index):
+        thislineone = []
         
-
-
-
+        for x in range(1, 7):
+            if x % 2 == 0:
+                thislineone.append(int(self.full_field[row_index+1, x-1]))
+            else:
+                thislineone.append(int(self.full_field[row_index][x-1]))
+        print(thislineone)
 
 def main():
     x = Reels()
     y = PlayingField()
     y.generate_field()
     x.generate_reel()
+    y.printaline(0)
 
 
 if __name__ == "__main__":
