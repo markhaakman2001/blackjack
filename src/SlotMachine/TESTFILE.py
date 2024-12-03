bda5dd2580c79194d748352f1800ccce1f235510
markhaakman2001 <135602514+markhaakman2001@users.noreply.github.com>
2024-12-03 23:32:25 +0100

    animation group
    animation group om die animaations tegelijk af te spelen.

--- src/SlotMachine/TESTFILE.py
+++ src/SlotMachine/TESTFILE.py
@@ -1,7 +1,7 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import Slot, QObject, Signal, QPropertyAnimation, QPoint, QEasingCurve, QSize, Qt
 import PySide6.QtCore as Core
-from PySide6.QtCore import QRect, QPropertyAnimation, Property
+from PySide6.QtCore import QRect, QPropertyAnimation, Property, QParallelAnimationGroup, QSequentialAnimationGroup
 from src.SlotMachine.slot_generator import Reels, PlayingField
 import time
 from math import *
@@ -28,15 +28,17 @@ class TestWindow(QtWidgets.QMainWindow):
         self.start_btn.clicked.connect(self.displayreel)
 
         self.playingfield = PlayingField()
+        self.animationgroup = QParallelAnimationGroup()
         
     
     def displayreel(self):
+        
         self.playingfield.generate_field()
         for i, reel in enumerate(self.playingfield.reels):
             text = reel.reel_disp
-            x = 100 + i * 50
+            x = 200 + i * 50
             self.textinwindow(text, x)
-            self.startanimation()
+            
 
 
     
@@ -44,42 +46,30 @@ class TestWindow(QtWidgets.QMainWindow):
         
         self.labels = []
         self.anims = []
+        
 
         for i, letter in enumerate(text):
             ypos = i*75
             label = QtWidgets.QLabel(letter)
             label.setParent(self.central_widget)
 
-            label.pos = QPoint(100, ypos)
+            label.pos = QPoint(xpos, ypos)
             label.move(label.pos)
+            label.setStyleSheet('background-color:#304; font-size:20px; font-weight:bold; color:#fff;')
             label.show()
             self.labels.append(label)
             anim = QPropertyAnimation(label, b"pos")
             anim.setStartValue(QPoint(xpos, 0))
             anim.setEndValue(QPoint(xpos, ypos))
-            anim.setDuration(500)
+            anim.setDuration(500 + 2 * xpos)
             self.anims.append(anim)
-        
-        #self.startanimation()
-    
-    def startanimation(self):
-        for anim in self.anims:
-            anim.start()
-            
-            
-            
+            self.animationgroup.addAnimation(anim)
+            self.animationgroup.start()
+
     
 
-            
-            
-            
-            
-            
-            
-            
-
-
-
+    def startanimationgroup(self):
+        self.animationgroup.start()
 
 
 app = QtWidgets.QApplication(sys.argv)
