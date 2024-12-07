887079af6e94359f408445b1ad9285939204142e
markhaakman2001 <135602514+markhaakman2001@users.noreply.github.com>
2024-12-07 15:45:41 +0100

    bijna bij een oplossing (zie uitleg)
    " RuntimeError: Internal C++ object (PySide6.QtCore.QPropertyAnimation) already deleted. " dit MOET opgelost worden, iets met stacked widgets ofso. Kijk ff bij def displaywinnersnew() line 108, of misschien def createwinanimation() line 129

--- src/SlotMachine/TESTFILE.py
+++ src/SlotMachine/TESTFILE.py
@@ -33,109 +33,158 @@ class TestWindow(QtWidgets.QMainWindow):
         # initialise the slot generator
         self.playingfield = PlayingField()
         self.animationgroup = QParallelAnimationGroup()
+        # self.anim_group = QParallelAnimationGroup()
+        self.fallanimationgroup = QParallelAnimationGroup()
 
         # Need label array to animate winning lines
         self.label_array = None
         labels = []
-
+        self.first = True
         # Label array for reusing labels instead of making new ones
         for x in range(6):
-            
+            xpos = 200 + x * 80
             if x == 0:
-                self.label_array = self.createlabelarray()
+                self.label_array = self.createlabelarray(xpos)
             else:
-                arr = self.createlabelarray()
+                arr = self.createlabelarray(xpos)
                 self.label_array = np.column_stack((self.label_array, arr))
+                
+        
+        self.createfallanimation()
+        self.createwinanimation()
         
 
             
 
-    def createlabelarray(self):
-        labels = []
-        for x in range(5):
-            lbl = CustomLabels()
-            lbl.setParent(self.central_widget)
-            labels.append(lbl)
-        arr = np.array(labels)
+    def createlabelarray(self, xpos):
+        self.labels = []
+        for y in range(5):
+            ypos = y * 96
+            self.lbl = CustomLabels()
+            self.lbl.setParent(self.central_widget)
+            self.lbl.setVisible(False)
+            self.lbl.setpos(QPoint(xpos, ypos))
+            self.lbl.setshiftpos(QPoint(xpos + 40, ypos + 48))
+            
+            self.labels.append(self.lbl)
+        arr = np.array(self.labels)
         return arr
 
 
     
     def displayreel(self):
+        
         self.animationgroup = QParallelAnimationGroup()
-
+        self.anim_group = [QParallelAnimationGroup(), QParallelAnimationGroup()]
+        self.printvisibility()
         # Generate a new random array of values
         self.playingfield.generate_field()
+        #self.start_btn.setEnabled(False)
         for i, reel in enumerate(self.playingfield.reels):
             text = reel.reel_disp
             x = 200 + i * 80
             self.textinwindownew(text, x, i)
         
         self.displaywinnersnew()
+        #self.animationgroup.finished.connect(self.displaywinnersnew)
+        self.first = False
+        
         # self.displaywinners()
+    
 
+    def printvisibility(self):
+        this_arr = np.empty((5, 6))
+        for x in range(5):
+            for y in range(6):
+                thislabel: CustomLabels = self.label_array[x, y]
+                this_arr[x, y] = thislabel.windowOpacity()
+        print(this_arr)
+
+    @Slot()
+    def enablestart(self):
+        self.start_btn.setEnabled(True)
 
 
     def displaywinnersnew(self):
         straight_arr, zigzag_arr = self.playingfield.checkwinnings()
         arrlist = [straight_arr, zigzag_arr]
         
-
-        for linearray in arrlist:
+        
+        for i, linearray in enumerate(arrlist):
             
             if np.any(linearray):
-                
-                winarray = self.label_array[np.asarray(linearray).nonzero()]
-                thisarray = (linearray).nonzero()
-                
                 anims = []
-                self.anim_group = QParallelAnimationGroup()
-
                 
                 for x in np.argwhere(linearray):
-                    label = self.label_array[x[0]][x[1]]
+                    label: CustomLabels = self.label_array[x[0]][x[1]]
                     #print(i)
-                    if not label.isanimated:
-                        win_anim = QPropertyAnimation(label, b"geometry")
-                        win_anim.setStartValue(QRect(label.shiftedpos, QSize(0, 0)))
-                        win_anim.setEndValue(QRect(label.currentpos, QSize(80, 96)))
-                        # win_anim.setKeyValueAt(0, QRect(label.shiftedpos, QSize(0, 0)))
-                        # win_anim.setKeyValueAt(0.25,QRect(label.currentpos, QSize(80, 96)))
-                        # win_anim.setKeyValueAt(0.60, QRect(label.shiftedpos, QSize(0, 0)))
-                        # win_anim.setKeyValueAt(1, QRect(label.currentpos, QSize(80, 96)))
-                        win_anim.setDuration(800)
-                        self.anim_group.addAnimation(win_anim)
-                        anims.append(win_anim)
-                        
                     
-                self.anim_group.start()
+                    self.anim_group[i].addAnimation(label.animation2)
+                    
+                        
+                self.anim_group[i].start()
 
 
+
+    def createfallanimation(self):
+        for x in range(5):
+            for y in range(6):
+                xpos = 200 + y * 80
+                ypos = x * 96
+                label: CustomLabels = self.label_array[x, y]
+                label.animation.setStartValue(QPoint(xpos, 0))
+                label.animation.setEndValue(QPoint(xpos, ypos))
+                label.animation.setDuration(100 + xpos)
+                self.fallanimationgroup.addAnimation(label.animation)
     
+
+    def createwinanimation(self):
+        for x in range(5):
+            for y in range(6):
+                label: CustomLabels = self.label_array[x, y]
+                label.animation2.setKeyValueAt(0, QRect(label.shiftedpos, QSize(0, 0)))
+                label.animation2.setKeyValueAt(0.25,QRect(label.currentpos, QSize(80, 96)))
+                label.animation2.setKeyValueAt(0.60, QRect(label.shiftedpos, QSize(0, 0)))
+                label.animation2.setKeyValueAt(1, QRect(label.currentpos, QSize(80, 96)))
+                label.animation2.setDuration(800)
+                
+                
+        
+        
+
+
+
+
+
     def textinwindownew(self, text, xpos, index):
         self.anims = []
         # self.animationgroup = QParallelAnimationGroup()
         for i, letter in enumerate(text):
             ypos = i*96
-
+            
             # Custom label with image
-            label: CustomLabels = self.label_array[i, index]
-            label.setnewimage(str(letter))
-            label.show()
-            label.isnotanimated()
+            self.label: CustomLabels = self.label_array[i, index]
+            self.label.clear()
+            
+            self.label.setnewimage(str(letter))
+            self.label.setVisible(True)
+            self.label.show()
+            self.label.isnotanimated()
+        
             
             # Create animation
-            anim = QPropertyAnimation(label, b"pos")
-            anim.setStartValue(QPoint(xpos, 0))
-            anim.setEndValue(QPoint(xpos, ypos))
-            label.setpos(QPoint(xpos, ypos))
-            label.setshiftpos(QPoint(xpos + 40, ypos + 48))
-            anim.setDuration(100 + xpos)
-
-            self.anims.append(anim)
-            self.animationgroup.addAnimation(anim)
-            self.animationgroup.start()
-            print(anim.group)
+            # self.anim = QPropertyAnimation(self.label, b"pos")
+            # self.anim.setStartValue(QPoint(xpos, 0))
+            # self.anim.setEndValue(QPoint(xpos, ypos))
+            
+            # self.anim.setDuration(100 + xpos)
+            
+            # self.anims.append(self.anim)
+            # self.animationgroup.addAnimation(self.anim)
+            self.fallanimationgroup.start()
+            
+            
+            
 
     def startanimationgroup(self):
         self.animationgroup.start()
@@ -151,6 +200,8 @@ class CustomLabels(QtWidgets.QLabel):
         self.animated = False
         self.pathname = "src/SlotMachine/images/"
         self.pixmap1 = QPixmap()
+        self.animation = QPropertyAnimation(self, b"pos")
+        self.animation2 = QPropertyAnimation(self, b"geometry")
         self.width = 80
         self.height = 96
         self.setMaximumWidth(self.width)
