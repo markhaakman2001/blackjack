192bf29c63d4ec1b1ad020f4d35f96ea719b8095
markhaakman2001 <135602514+markhaakman2001@users.noreply.github.com>
2025-02-02 22:07:26 +0100

    start on global bank
    started a new folder for a global bank.
    This bank is used to keep track of the players balance across all games and will be used for deposits in future updates.

---
+++ src/UnifiedBanking/UnifiedBank.py
@@ -0,0 +1,38 @@
+class MainBank:
+
+    def __init__(self):
+        self._FundsCredits : int = 0
+    
+    def DepositMoney(self, AmountEuros : float):
+        AmountCredits         = int(AmountEuros * 100)
+        self._BalanceCredits_ = AmountCredits
+    
+    @property
+    def _BalanceEuros_(self) -> float:
+        """Get the balance in euros. (1 euro = 100 credits)
+
+        Returns:
+            float: Current account balance in EUROS
+        """       
+        _FundsEuros_ = (self._BalanceCredits_ / 100)
+        return _FundsEuros_
+    
+    @property
+    def _BalanceCredits_(self) -> int:
+        """Get account balance in CREDITS (100 credits = 1 euro)
+
+        Returns:
+            _type_: _description_
+        """        
+        return self._FundsCredits
+    
+    @_BalanceCredits_.setter
+    def _BalanceCredits_(self, AmountCredits : int) -> None:
+        """Change the balance by a certain amount
+
+        Args:
+            AmountCredits (int): How much credits should be added to the balance
+        """             
+        OldFunds = self._FundsCredits
+        NewFunds = OldFunds + AmountCredits
+        self._FundsCredits = NewFunds
