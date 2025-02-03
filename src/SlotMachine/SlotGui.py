bd3641d2282f331dbebc2c9e5c18f05a21290c8c
markhaakman2001 <135602514+markhaakman2001@users.noreply.github.com>
2025-02-03 19:29:15 +0100

    --

--- src/SlotMachine/SlotGui.py
+++ src/SlotMachine/SlotGui.py
@@ -1,14 +1,12 @@
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QStyleOption, QStyle
-from PySide6.QtCore import Slot, QObject, Signal, QPropertyAnimation, QPoint, QEasingCurve, QSize, Qt
+from PySide6.QtCore import Slot, Signal, QPropertyAnimation, QPoint, QSize
 import PySide6.QtCore as Core
 from PySide6.QtCore import QRect, QPropertyAnimation, Property, QParallelAnimationGroup, QSequentialAnimationGroup, QAbstractAnimation
 from src.SlotMachine.slot_generator import Reels, PlayingField, BankAccount
-from PySide6.QtGui import QImageReader, QImage, QPixmap, QPicture
+from PySide6.QtGui import QPixmap
 from src.SlotMachine.ExtraSlotFiles import SlotWinType
-from src.ErrorFiles.BankingErrors import BalanceError, ZeroFundsError, InsufficientFundsError, BankingErrorChecker
+from src.ErrorFiles.BankingErrors import BalanceError
 import numpy as np
-import random
 import sys
 
 
